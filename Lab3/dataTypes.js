7d5a9edbb4b95bc0b4f7def343703387f601f7a2
Aziza0508 <112758271+Aziza0508@users.noreply.github.com>
2024-02-14 20:02:06 +0600

    first commit

---
+++ Lab3/dataTypes.js
@@ -0,0 +1,37 @@
+let n = 123;
+n = 12.345;
+// number  data types include everything
+
+alert( 1 / 0 );
+// Infinity
+
+alert( "not a number" / 2 );
+// NaN not a number prikin'
+
+console.log(9007199254740991 + 1);
+// bit int
+
+let str = "Hello";
+let str2 = 'Single quotes are ok too';
+let phrase = `can embed another ${str}`;
+// Strings 
+
+let name1 = "John";
+alert( `Hello, ${name}!`)
+// embed a variable, or an expression
+
+
+let isGreater = 4 > 1;
+alert( isGreater );
+//boolean
+
+
+
+// task
+let name = "Aziza";
+
+alert( `hello ${1}` ); // hello 1
+
+alert( `hello ${"name"}` ); // hello name
+
+alert( `hello ${name}` ); // hello Aziza
---
+++ Lab3/vars.js
@@ -0,0 +1,28 @@
+let message;
+message = 'Hello!';
+
+alert(message);
+
+let user = 'John',
+  age = 25,
+  message2 = 'Hello';
+// variable can be declared oly once. i.e you can't use let twice on a same variable
+
+const myBirthday = '09.08.2005';s
+
+
+
+// task 1
+var admin;
+var name;
+name = "John";
+admin = name;
+alert(admin)
+
+// task 2
+let ourPlanet = "Earth"
+let currentUserName = "John";
+
+// task 3
+const BIRTHDAY = '18.04.1982';
+const age1 = someCode(BIRTHDAY);
